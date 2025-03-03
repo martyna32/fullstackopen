70ae636feace75fe88a505661f12fce8d4ebb2fd
marti <martynciahotuwa@gmail.com>
2025-03-03 10:34:59 +0100

    first commit

---
+++ src/components/Total.jsx
@@ -0,0 +1,7 @@
+const Total = ({ parts }) => {
+    const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
+    return <p><strong>Total of {totalExercises} exercises</strong></p>;
+  };
+  
+  export default Total;
+  
